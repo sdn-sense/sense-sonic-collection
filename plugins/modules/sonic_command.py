046535d0aeaa5829d5b50ce3af05a219c3cc4008
Justas Balcas <juztas@gmail.com>
2024-07-16 09:23:21 -0500

    Update ping and traceroute calls to sonic

--- plugins/modules/sonic_command.py
+++ plugins/modules/sonic_command.py
@@ -3,42 +3,14 @@
 """
 SENSE Azure Sonic Module, which is copied and called via Ansible from
 SENSE Site-RM Resource Manager.
-
-Main reasons for this script are the following:
-    1. Azure Sonic does not have Ansible module
-    2. Dell Sonic module depends on sonic-cli - and currently (140422) -
-       it is broken due to python2 removal. See https://github.com/Azure/SONiC/issues/781
-    3. It is very diff from normal switch cli, like:
-          If vlan present on Sonic, adding it again will raise Exception (on Dell/Arista Switches, it is not)
-          If vlan not cleaned (has member, ip, or any param) Sonic does not allow to remove vlan. First need to
-          clean all members, params, ips and only then remove vlan.
-    4. For BGP - We cant use SONiC config_db.json - as it is not rich enough, and does not support all features
-       (route-map, ip list). Because of this - we have to rely on vtysh
-
-With this script - as input, it get's information from Site-RM for which vlan and routing to configure/unconfigure
-It checks with local configuration and applies the configs on Sonic with config command or routing with vtysh
-
-Authors:
-  Justas Balcas jbalcas (at) caltech.edu
-
-Date: 2022/04/14
 """
 import ast
-import ipaddress
 import json
 import os
 import re
 import shlex
 import subprocess
 import sys
-
-def normalizeIPAddress(ipInput):
-    """Normalize IP Address"""
-    tmpIP = ipInput.split("/")
-    longIP = ipaddress.ip_address(tmpIP[0]).exploded
-    if len(tmpIP) == 2:
-        return f"{longIP}/{tmpIP[1]}"
-    return longIP
 
 
 def externalCommand(command):
@@ -57,19 +29,6 @@ def externalCommand(command):
     return ["", "", -1]
 
 
-def sendviaStdIn(maincmd, commands):
-    """Send commands to maincmd stdin"""
-    if not isinstance(maincmd, list):
-        maincmd = shlex.split(maincmd)
-    with subprocess.Popen(
-        maincmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE
-    ) as mainProc:
-        singlecmd = ""
-        for cmd in commands:
-            singlecmd += f"{cmd}\n"
-        mainProc.communicate(input=singlecmd.encode())
-
-
 def strtojson(intxt):
     """str to json function"""
     out = {}
@@ -81,6 +40,12 @@ def strtojson(intxt):
         raise Exception(f"SyntaxError: Failed to literal eval dict. Err:{ex} ") from ex
     return out
 
+def listtostr(inlist):
+    """List to Str conversation"""
+    out = ""
+    for line in inlist:
+        out += f"{str(line)}\n"
+    return out
 
 def loadJson(infile):
     """Load json file and return dictionary"""
@@ -103,17 +68,83 @@ class Main:
         self.args = None
         self.module_stdout = []
         self.module_stderr = []
+        self.rc = 0
 
-    def execute(self):
+    def log_out(self, out):
+        """Log all output to stdout and stderr. Set RC code of command exit"""
+        for line in out[0].decode("utf-8").split("\n"):
+            self.module_stdout.append(line)
+        for line in out[1].decode("utf-8").split("\n"):
+            self.module_stderr.append(line)
+        self.rc = out[2]
+
+    def execute_ping(self, pingconf):
+        """Execute ping command"""
+        count = pingconf.get('count', 10)  # Default to 10
+        timeout = pingconf.get('timeout', 5)  # Default to 5 seconds
+        ipv4_address = pingconf.get('ipv4_address', '')
+        ipv6_address = pingconf.get('ipv6_address', '')
+        ping_type = pingconf.get('type', None)
+
+        # Construct the ping command based on the type
+        if ping_type == 'ipv4':
+            address = ipv4_address
+            ping_command = f"ping -c {count} -W {timeout} {address}"
+        elif ping_type == 'ipv6':
+            address = ipv6_address
+            ping_command = f"ping6 -c {count} -W {timeout} {address}"
+        else:
+            raise ValueError("Unsupported type. Only 'ipv4' and 'ipv6' are supported.")
+        # Execute command and get output
+        try:
+            out = externalCommand(ping_command)
+        except Exception as ex:
+            raise Exception(f"Failed execute command {ping_command}. Exception {ex}") from ex
+        self.log_out(out)
+
+
+    def execute_traceroute(self, traceconf):
+        """Execute traceroute command"""
+        ipv4_address = traceconf.get('ipv4_address', '')
+        ipv6_address = traceconf.get('ipv6_address', '')
+        trace_type = traceconf.get('type', 'ipv4')
+
+        # Construct the traceroute command based on the type
+        if trace_type == 'ipv4':
+            address = ipv4_address
+            traceroute_command = f"traceroute {address}"
+        elif trace_type == 'ipv6':
+            address = ipv6_address
+            traceroute_command = f"traceroute6 {address}"
+        else:
+            raise ValueError("Unsupported type. Only 'ipv4' and 'ipv6' are supported.")
+        # Execute command and get output
+        try:
+            out = externalCommand(traceroute_command)
+        except Exception as ex:
+            raise Exception(f"Failed execute command {traceroute_command}. Exception {ex}") from ex
+        self.log_out(out)
+
+    def execute(self, action):
         """Main execute"""
-        senseconfig = loadJson(self.args["config"])
-        print(senseconfig)
+        senseconfig = loadJson(self.args[action])
+        if action == "ping":
+            self.module_stdout.append(f"Execute ping: {senseconfig.get('PING', None)}")
+            self.execute_ping(senseconfig.get('PING', None))
+        elif action == "traceroute":
+            self.module_stdout.append(f"Execute traceroute: {senseconfig.get('TRACEROUTE', None)}")
+            self.execute_traceroute(senseconfig.get('TRACEROUTE', None))
+        else:
+            self.module_stderr.append(f"Unknown action {action}")
+            raise Exception(f"Unknown action {action}")
 
     def parseArgs(self, inFile):
         """Parse Args from input file"""
         if not os.path.isfile(inFile):
             raise Exception("Input File from param does not exist on Device.")
-        params = {"debug": r"sonic_debug=(\S+)", "config": r"sonic_config=(\S+)"}
+        params = {"debug": r"sonic_debug=(\S+)",
+                  "ping": r"sonic_ping=(\S+)",
+                  "traceroute": r"sonic_traceroute=(\S+)"}
         args = {}
         with open(inFile, "r", encoding="utf-8") as fd:
             tmptxt = fd.read()
@@ -123,15 +154,33 @@ class Main:
                     args[key] = match[1]
         return args
 
-
-    def main(self):
+    def mainwrap(self):
+        """Main wrapper for call"""
         if len(sys.argv) != 2:
             raise Exception(f"Issue with passed arguments. Input: {sys.argv}")
         self.args = self.parseArgs(sys.argv[1])
-        if not self.args.get("config", None):
+        action = None
+        if self.args.get("ping", None):
+            self.module_stdout.append(self.args["ping"])
+            self.module_stderr.append(self.args["ping"])
+            action = "ping"
+        elif self.args.get("traceroute", None):
+            self.module_stdout.append(self.args["traceroute"])
+            self.module_stderr.append(self.args["traceroute"])
+            action = "traceroute"
+        else:
+            self.module_stderr.append(f"Issue with parsing input config. Input: {sys.argv}")
             raise Exception(f"Issue with parsing input config. Input: {sys.argv}")
-        self.execute()
-        print(json.dumps({"changed": "ok"}))
+        self.execute(action)
+    def main(self):
+        """Main call executed by ansible"""
+        try:
+            self.mainwrap()
+        except Exception as ex:
+            self.module_stderr.append(f"Received exception running script. Ex: {ex}")
+            self.rc = 1
+        out = {'stdout': listtostr(self.module_stdout), 'stderr': listtostr(self.module_stderr), 'rc': self.rc, 'changed': False}
+        print(json.dumps(out))
 
 
 if __name__ == "__main__":
